160711d37483bace38a592d5e620060418c7c484
Gamal Eldeen Ahmed Khalaf <superzoro23@yahoo.com>
2019-04-22 22:19:51 +0200

    modifying regex

--- main.cpp
+++ main.cpp
@@ -6,17 +6,44 @@ using namespace std;
 int main()
 {
     /*initializing the map*/
-    map<string,int> opCode;
-    opCode.insert({"ADD",0x18});opCode.insert({"ADDR",0x90});opCode.insert({"CLEAR",0xB4});opCode.insert({"COMP",0x18});
-    opCode.insert({"COMPR",0xA0});opCode.insert({"DIV",0x24});opCode.insert({"DIVR",0x9C});opCode.insert({"J",0x3C});
-    opCode.insert({"JEQ",0x30});opCode.insert({"JLT",0x38});opCode.insert({"JGT",0x34});opCode.insert({"JSUB",0x48});
-    opCode.insert({"LDA",0x00});opCode.insert({"LDB",0x68});opCode.insert({"LDCH",0x50});opCode.insert({"LDL",0x08});
-    opCode.insert({"LDS",0x6C});opCode.insert({"LDT",0x74});opCode.insert({"LDX",0x04});opCode.insert({"MUL",0x20});
-    opCode.insert({"MULR",0x98});opCode.insert({"RMO",0xAC});opCode.insert({"RSUB",0x4C});opCode.insert({"COMP",0x18});
-    opCode.insert({"STA",0x0C});opCode.insert({"STB",0x78});opCode.insert({"STCH",0x54});opCode.insert({"STL",0x14});
-    opCode.insert({"STS",0x7C});opCode.insert({"STT",0x84});opCode.insert({"STX",0x10});opCode.insert({"SUB",0x1C});
-    opCode.insert({"SUBR",0x94});opCode.insert({"RD",0xD8});opCode.insert({"TD",0xE0});opCode.insert({"WD",0xDC});
-    opCode.insert({"TIX",0x2C});opCode.insert({"TIXR",0xB8});
+    map<string,string> opCode;
+    opCode.insert({"ADD","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"ADDR","^[ABLSTX]\\,[ABLSTX]$}"});
+    opCode.insert({"CLEAR","^[ABLSTX]$"});
+    opCode.insert({"COMP","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"COMPR","^[ABLSTX]\\,[ABLSTX]$}"});
+    opCode.insert({"DIV","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"DIVR","^[ABLSTX]\\,[ABLSTX]$}"});
+    opCode.insert({"J","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"JEQ","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"JLT","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"JGT","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"JSUB","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"LDA","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"LDB","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"LDCH","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"LDL","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"LDS","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"LDT","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"LDX","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"MUL","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"MULR","^[ABLSTX]\\,[ABLSTX]$}"});
+    opCode.insert({"RMO","^[ABLSTX]\\,[ABLSTX]$}"});
+    opCode.insert({"RSUB","^$"});
+    opCode.insert({"STA","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"STB","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"STCH","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"STL","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"STS","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"STT","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"STX","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"SUB","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"SUBR","^[ABLSTX]\\,[ABLSTX]$}"});
+    opCode.insert({"RD","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"TD","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"WD","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"TIX","^((\\@[a-zA-Z0-9]{1,17})|(\\#[a-zA-Z0-9]{1,17})|([a-zA-Z0-9]{1,18}))$"});
+    opCode.insert({"TIXR","^[ABLSTX]$}"});
 
     /*READING FROM FILE
     ===================*/
@@ -29,7 +56,7 @@ int main()
         }
         inputFile.close();
     }
-    regex r{"^([a-zA-z0-9]+)?(\\s*?)([+]?[A-Z]+)(\\s+)([@|#]?([a-zA-z0-9]+)|([a-zA-z0-9]+\\,{1}[X])|([ABLSTX]\\,{1}[ABLSTX]))$"};
+    regex r{"^([a-zA-z0-9]{1,8})?(\\s*?)((\\+[A-Z]{1,5})|([A-Z]{1,6}))(\\s+)([@|#]?([a-zA-z0-9]+)|([a-zA-z0-9]+\\,{1}[X])|([ABLSTX]\\,{1}[ABLSTX]))$"};
     if(regex_match(lines.at(0),r)){
             cout<<"MATCH";
     }
